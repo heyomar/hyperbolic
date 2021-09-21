ac0bfe97946845dacb4488cd83c740c65bd079c1
heyomar <25757036+heyomar@users.noreply.github.com>
2021-09-20 20:40:13 -0500

    packages, formatting, layout for site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 module.exports = {
-	purge: ['app/*.html'],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			backgroundImage: (theme) => ({
-				mamalola: "url('/app/assets/bg-mamalola.jpg')",
-				waterboys: "url('/app/assets/bg-waterboys.jpg')",
-				plebanlaw: "url('/app/assets/bg-plebanlaw.jpg')",
-				genusmedical: "url('/app/assets/bg-genusmedical.png')",
-				monsanto: "url('/app/assets/bg-monsanto.jpg')",
-			}),
-		},
-		fontFamily: {
-			sans: ['Rubik', 'ui-sans-serif', 'system-ui'],
-			body: ['Rubik'],
-		},
-		container: {
-			center: true,
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+    // mode: 'jit',
+    purge: ['app/*.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            backgroundImage: (theme) => ({
+                mamalola: "url('/app/assets/bg-mamalola.jpg')",
+                waterboys: "url('/app/assets/bg-waterboys.jpg')",
+                plebanlaw: "url('/app/assets/bg-plebanlaw.jpg')",
+                genusmedical: "url('/app/assets/bg-genusmedical.png')",
+                monsanto: "url('/app/assets/bg-monsanto.jpg')",
+            }),
+        },
+        fontFamily: {
+            sans: ['Rubik', 'ui-sans-serif', 'system-ui'],
+            body: ['Rubik'],
+        },
+        container: {
+            center: true,
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
