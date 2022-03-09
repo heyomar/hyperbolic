e8ffcf6e80ee436e49d0a6334a291a0de33bbab5
heyomar <25757036+heyomar@users.noreply.github.com>
2022-03-08 23:00:16 -0600

    add ea, utex, tmo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
 module.exports = {
-    content: ['app/*.html', '/app/assets/*.svg'],
-    theme: {
-        extend: {
-            backgroundImage: (theme) => ({
-                mamalola: "url('/app/assets/bg-mamalola.jpg')",
-                waterboys: "url('/app/assets/bg-waterboys.jpg')",
-                plebanlaw: "url('/app/assets/bg-plebanlaw.jpg')",
-                genusmedical: "url('/app/assets/bg-genusmedical.png')",
-                monsanto: "url('/app/assets/bg-monsanto.jpg')",
-                capitalone: "url('/app/assets/bg-capitalone.jpg')",
-                capitalone_sobe: "url('/app/assets/bg-capitalone-sobe.png')",
-            }),
-        },
-        fontFamily: {
-            sans: ['Lexend Deca', 'ui-sans-serif', 'system-ui'],
-            body: ['Lexend Deca'],
-        },
-        container: {
-            center: true,
-        },
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+	content: ['app/*.html', '/app/assets/*.svg'],
+	theme: {
+		extend: {
+			backgroundImage: (theme) => ({
+				mamalola: "url('/app/assets/bg-mamalola.jpg')",
+				waterboys: "url('/app/assets/bg-waterboys.jpg')",
+				plebanlaw: "url('/app/assets/bg-plebanlaw.jpg')",
+				genusmedical: "url('/app/assets/bg-genusmedical.png')",
+				monsanto: "url('/app/assets/bg-monsanto.jpg')",
+				capitalone: "url('/app/assets/bg-capitalone.jpg')",
+				capitalone_sobe: "url('/app/assets/bg-capitalone-sobe.png')",
+				utex: "url('/app/assets/bg-utex.jpg')",
+				ea: "url('/app/assets/bg-ea.png')",
+				next: "url('/app/assets/bg-tmobile.jpg')",
+			}),
+		},
+		fontFamily: {
+			sans: ['Lexend Deca', 'ui-sans-serif', 'system-ui'],
+			body: ['Lexend Deca'],
+		},
+		container: {
+			center: true,
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
